ae06664db6bf77b72f5c1a7b010de1cb558c99d7
Jim Howes <das.howes@gmail.com>
2016-05-13 19:49:18 +0000

    Static pages 100

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,21 +4,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Geoffrey C. Howes"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Geoffrey C. Howes"
   end
 
   test "should get bibliography" do
     get :bibliography
     assert_response :success
+    assert_select "title", "Bibliography | Geoffrey C. Howes"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contact | Geoffrey C. Howes"
   end
 
 end
